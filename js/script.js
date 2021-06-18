f932d82558bc1331038710253000d49042fbb09c
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-06-18 10:50:01 +0200

    add grafic

---
+++ js/script.js
@@ -0,0 +1,3 @@
+//TODO Milestone 1 Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout. 
+//TODO Milestone 2 Coloriamo le icone per tipo
+//TODO Milestone 3 Creiamo una select con i tipi di icone e usiamola per filtrare le icone
